21887e596f775c25d308fdc270ad3afa89449843
kajc10 <34817244+kajc10@users.noreply.github.com>
2021-04-14 19:41:01 +0200

    Update GT4500.java
    added a missing break;

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -84,6 +84,7 @@ public class GT4500 implements SpaceShip {
         if (! secondaryTorpedoStore.isEmpty()) {
             firingSuccess = secondaryTorpedoStore.fire(1);
          }
+        break;
 
     }
 
